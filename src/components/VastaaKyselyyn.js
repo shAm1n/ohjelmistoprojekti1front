683aa761ede598f24b8173857369d923098f31ae
shAm1n <minna.marianne.shalaby@gmail.com>
2022-05-12 11:05:01 +0300

    Lomake toimii!

--- src/components/VastaaKyselyyn.js
+++ src/components/VastaaKyselyyn.js
@@ -1,49 +1,87 @@
-import React, { useState }  from 'react';
-import {useParams} from 'react-router-dom';
-import {Box, FormGroup, FormControl, TextField, InputLabel, Button, Typography} from '@mui/material';
+import React, { useState, useEffect }  from 'react';
+import { useParams } from 'react-router-dom';
+import {Box, FormControl, TextField, InputLabel, Button, Typography, Alert} from '@mui/material';
 
 export default function VastaaKyselyyn(props) {
+    const [error, setError] = useState(null);
+    const [viesti, setViesti] = useState('');
+    const [loading, setLoading] = useState(true);
     const {id} = useParams();
-    const [vastaus, setVastaus] = useState([]);
+    const [kysymykset, setKysymykset] = useState([]);
+    const [vastaus, setVastaus] = useState(
+        {
+            vastaus: '',
+            kysymys: {kysymysid: null},
+        }
+    );
 
-    const muuta = (e) => {
-        setVastaus(e.target.value);
+    useEffect(() => {
+    const haeKysymykset = () => {
+        fetch(`/api/kyselyt/${id}`)
+        .then(response => response.json())
+        .then(data => {
+          setLoading(false);
+          setKysymykset(data.kysymykset);
+        },
+          (error) => {
+          setLoading(false);
+          setError(error);
+        })
+    };
+    haeKysymykset();
+    }, [id])
+
+    const muuta = (e, id) => {
+        setViesti('')
+        setVastaus({
+            vastaus : e.target.value,
+            kysymys: {kysymysid : id},
+        });
     }
-
-    const lisaaVastaukset = (vs) => {
+//e.target.getAttribute('data-key')
+    /*const lisaaVastaukset = (vs) => {
         vs.preventDefault();
         setVastaus([]);
+    }*/
+
+    async function lahetaVastaus() {
+        console.log('Vs:'+JSON.stringify(vastaus))
+        const response = await fetch('/api/vastaukset', {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify(vastaus)
+        }).catch(err => console.error(err))
+        if (response.status === 200) {
+            setViesti('Vastauksen lähetys onnistui!');
+        } else {
+            setViesti('Virhe tiedon lähetyksessä');
+        }
     }
 
-    const makeForm = ((kysymykset) => {
+    /*const makeForm = (() => {
         return (
             <Box>
-            {kysymykset.map((kysy) => 
-                <FormControl key={kysy.kysymysid} sx={{margin:2, width: 400}}>
-                    <InputLabel htmlFor='kysy.vastaukset.vastaus'></InputLabel>
-                    <TextField name='kysy.vastaukset.vastaus' value={kysy.vastaukset.vastaus} onChange={(e) => muuta(e)} label={kysy.kysymyslaatikko} variant="outlined" focused/>
-                </FormControl>
-            )}
+            
             </Box>
         );
-    })
-
+    })*/
+    if(error) {
+        return <div>Error</div>
+    } else if(loading) {
+        return <div>Loading...</div>
+    } else {
     return (
-    <Box component='form' noValidate autoComplete='off' sx={{margin:2}}>
-        {props.lista.map(kysely => {
-            if (kysely.kyselyId===Number(id)) {
-            return (
-            <FormGroup sx={{width: 400}}>
-                {makeForm(kysely.kysymykset)}
-                <Button variant='outlined' onClick={lisaaVastaukset}>Valmis!</Button>
-            </FormGroup>
-            );
-            } else {
-            return (
-                <Typography></Typography>
-            );
-        }})}
-        <Typography>{vastaus}</Typography>
+    <Box sx={{margin:2}}>
+        {kysymykset.map((kysy) =>
+                <FormControl key={kysy.kysymysid} sx={{margin:2, width: 400}}>
+                <InputLabel htmlFor='kysy.vastaukset.vastaus'></InputLabel>
+                <TextField name='kysy.vastaukset.vastaus' data-key={kysy.kysymysid} value={kysy.vastaukset.vastaus} onChange={(e) => muuta(e, kysy.kysymysid)} label={kysy.kysymyslaatikko} variant="outlined" focused/>
+                <Button variant='outlined' onClick={lahetaVastaus}>Valmis!</Button>
+                </FormControl>
+        )}
+        <Alert>{viesti}</Alert>
+        <Typography>{vastaus.vastaus}</Typography>
     </Box>
     );
+    }
 }
