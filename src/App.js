4fdf7eadf6141edff26dac77aab8277109bc006f
Troksii <santtux2@hotmail.com>
2022-05-17 13:16:57 +0300

    vika versio

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ export default function App() {
   const [lista, setLista] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  //Fetch komento että saadaan yhteys apiin
   useEffect(() => {
     fetch('/api/kyselyt', {
     method: 'GET',
